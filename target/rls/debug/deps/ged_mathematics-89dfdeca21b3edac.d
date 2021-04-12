b55e73ab07febe3ca2a1846f82bc836b10578f07
danite <luansnain@gmail.com>
2021-04-12 17:56:17 +0200

    Change project to be one whith modules

---
+++ target/rls/debug/deps/ged_mathematics-89dfdeca21b3edac.d
@@ -0,0 +1,5 @@
+d:\OSSU\GED-mathematics\target\rls\debug\deps\ged_mathematics-89dfdeca21b3edac.rmeta: src\main.rs
+
+d:\OSSU\GED-mathematics\target\rls\debug\deps\ged_mathematics-89dfdeca21b3edac.d: src\main.rs
+
+src\main.rs:
